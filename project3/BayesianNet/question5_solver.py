ea6a2fa3ce4ec59ecf048e54f586038036fc13bf
Varsha Paidi <varsha_paidi@hotmail.com>
2014-11-16 17:44:39 -0500

    Solved Question 5

--- project3/BayesianNet/question5_solver.py
+++ project3/BayesianNet/question5_solver.py
@@ -1,3 +1,4 @@
+import string
 class Question5_Solver:
     def __init__(self, cpt2):
         self.cpt2 = cpt2;
@@ -20,6 +21,98 @@ class Question5_Solver:
     # query example:
     #    query: "ques_ion";
     #    return "t";
-    def solve(self, query):
-        return "t";
 
+    def solve(self, query):
+        #To compare which letter gives max prob
+        max_prob = 0.0
+        #Probability
+        prob = 0.0
+
+        #To iterate through all the letters and calculate prob
+        #Skip the calculation of prob when char is "_".
+        for i, c in enumerate(query):
+            if c != "_" and i <= len(query)-1:
+                #To calculate the first probability
+                if i == 0:
+                    prob = self.cpt2.conditional_prob(query[i], "`", "`")
+                elif i == 1:
+                    prob = prob * self.cpt2.conditional_prob(query[i], "`", query[i - 1])
+                #To calculate the last probability
+                elif i == len(query)-1:
+                    prob = prob * self.cpt2.conditional_prob(query[i], query[i - 2], query[i - 1])
+                    prob = prob * self.cpt2.conditional_prob("`", query[i - 1], query[i])
+                    prob = prob * self.cpt2.conditional_prob("`", query[i], "`")
+                    max_prob = prob
+                #To calculate middle probabilities
+                else:
+                    if query[i - 1] == "_":
+                        continue
+                    if query[i - 2] == "_":
+                        continue
+                    else:
+                        prob = prob * self.cpt2.conditional_prob(query[i], query[i - 2], query[i - 1])
+                        max_prob = prob
+            else:
+                if i == 0 and len(query) == 2:
+                    letter_before_1 = "`"
+                    letter_before_2 = "`"
+                    letter_after_1 = query[i + 1]
+                    letter_after_2 = "`"
+                    prob = 1.0
+                elif i == 0:
+                    letter_before_1 = "`"
+                    letter_before_2 = "`"
+                    letter_after_1 = query[i + 1]
+                    letter_after_2 = query[i + 2]
+                    prob = 1.0
+                elif i == 1 and len(query) == 2:
+                    letter_before_1 = "`"
+                    letter_before_2 = query[i - 1]
+                    letter_after_1 = "`"
+                    letter_after_2 = "`"
+                elif i == 1 and len(query) == 3:
+                    letter_before_1 = "`"
+                    letter_before_2 = query[i - 1]
+                    letter_after_1 = query[i + 1]
+                    letter_after_2 = "`"
+                elif i == 1:
+                    letter_before_1 = "`"
+                    letter_before_2 = query[i - 1]
+                    letter_after_1 = query[i + 1]
+                    letter_after_2 = query[i + 2]
+                elif i == len(query)-1:
+                    letter_before_1 = query[i - 2]
+                    letter_before_2 = query[i - 1]
+                    letter_after_1 = "`"
+                    letter_after_2 = "`"
+                elif i == len(query)-2:
+                    letter_before_1 = query[i - 2]
+                    letter_before_2 = query[i - 1]
+                    letter_after_1 = query[i + 1]
+                    letter_after_2 = "`"
+                else:
+                    letter_before_2 = query[i - 1]
+                    letter_before_1 = query[i - 2]
+                    letter_after_1 = query[i + 1]
+                    letter_after_2 = query[i + 2]
+
+
+        #To iterate through all alphabets and check which gives the max prob
+        #Handling the case when char is "_"
+        alphs = string.ascii_lowercase
+        all_prob = {}
+
+        for i in alphs:
+            prob = max_prob * self.cpt2.conditional_prob(i, letter_before_1, letter_before_2)
+            prob = prob * self.cpt2.conditional_prob(letter_after_1, letter_before_2, i)
+            prob = prob * self.cpt2.conditional_prob(letter_after_2, i, letter_after_1)
+
+            if prob != 0.0:
+                all_prob[i] = prob
+
+        if len(all_prob.keys()) > 0:
+            missing_letter = max(all_prob, key=all_prob.get)
+        else:
+            missing_letter = "t"
+
+        return missing_letter
