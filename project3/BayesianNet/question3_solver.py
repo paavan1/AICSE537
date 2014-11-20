688f57bd3c2d723fd078bfc891a679546dd1a9ef
Varsha Paidi <varsha_paidi@hotmail.com>
2014-11-20 17:07:06 -0500

    Calculation of 1-hidden variable table

--- project3/BayesianNet/question3_solver.py
+++ project3/BayesianNet/question3_solver.py
@@ -1,5 +1,21 @@
 class Question3_Solver:
     def __init__(self, cpt):
+        alphs = ["`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+        #Below is the cpt for one hidden variable
+        #given characters are in rows
+        self.cpt3 = [[0 for x in range(27)] for x in range(27)]
+        sum_prob = 0
+
+        for left_let in alphs:
+            for right_let in alphs:
+                for mid_let in alphs:
+                    sum_prob += cpt.conditional_prob(mid_let, left_let) * cpt.conditional_prob(right_let, mid_let)
+                    i_right = ord(right_let) - 96
+                    i_left = ord(left_let) - 96
+                    self.cpt3[i_right][i_left] = sum_prob
+
         self.cpt = cpt;
 
     #####################################
@@ -15,5 +31,6 @@ class Question3_Solver:
     #    query: "qu--_--n";
     #    return "t";
     def solve(self, query):
+
         return "t";
 
