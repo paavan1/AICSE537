3e0e6554ad6d56e613593f14410c4541180b1f66
vivek pradhan <vivek.pradhan@stonybrook.edu>
2014-11-28 21:58:09 -0500

    q2 and q3 are over

--- project4/question2_solver.py
+++ project4/question2_solver.py
@@ -1,6 +1,9 @@
+import numpy
+import math
 class Question2_Solver:
     def __init__(self):
-        self.learn('train.data');
+        probabilities = self.learn('train.data');
+        self.probabilities = probabilities
         return;
 
     # Add your code here.
@@ -9,12 +12,68 @@ class Question2_Solver:
     # This function runs only once when initializing
     # Please read and only read train_data: 'train.data'
     def learn(self, train_data):
-        return;
-
+        #Initialize model with zero probabilities
+        probabilities = numpy.zeros((2,3,16))
+        for i in range(0,16):
+            #probabilities is a list with indices [democrat|republican][y|n|?][features]
+            probabilities[0][0][i] = 0
+            probabilities[0][1][i] = 0
+            probabilities[0][2][i] = 0
+            probabilities[1][0][i] = 0
+            probabilities[1][1][i] = 0
+            probabilities[1][2][i] = 0
+        #Read the data and populate counts
+        with open(train_data) as f:
+            content = f.readlines()
+        for data in content:
+            data = data.split()
+            if data[0] == 'democrat':
+                whichcrat = 0
+            if data[0] == 'republican':
+                whichcrat = 1
+            tempdata = data[1].split(',')
+            index = 0
+            for value in tempdata:
+                if value == 'y':
+                    probabilities[whichcrat][0][index] = probabilities[whichcrat][0][index] + 1
+                if value == 'n':
+                    probabilities[whichcrat][1][index] = probabilities[whichcrat][1][index] + 1
+                if value == '?':
+                    probabilities[whichcrat][2][index] = probabilities[whichcrat][2][index] + 1
+                index = index + 1
+        #normalize and take logs
+        numoftraindata = probabilities[0][0][0] + probabilities[0][1][0] + probabilities[0][2][0]
+        for i in range(0,2):
+            for j in range(0,3):
+                for k in range(0,16):
+                    probabilities[i][j][k] = (probabilities[i][j][k]+1)/(numoftraindata+16)
+        return probabilities;
+    
+    def calc_prob(self, query, whichcrat):
+        tempdata = query.split(',')
+        index = 0
+        prob_to_calc = 1
+        for value in tempdata:
+            if value == 'y':
+                prob_to_calc = prob_to_calc *self.probabilities[whichcrat][0][index]
+            if value == 'n':
+                prob_to_calc = prob_to_calc *self.probabilities[whichcrat][1][index]
+            if value == '?':
+                prob_to_calc = prob_to_calc *self.probabilities[whichcrat][2][index]
+            index = index + 1
+        return prob_to_calc;
+    
     # Add your code here.
     # Use the learned naive bayes classifier to predict
     # query example: 'n,y,n,y,y,y,n,n,n,y,?,y,y,y,n,y'
     # return 'republican' or 'republican'
     def solve(self, query):
-        return 'democrat';
-
+        prob = [0.0,0.0]
+        prob[0] = self.calc_prob(query,0)
+        prob[1] = self.calc_prob(query,1)
+        if prob[0] == prob[1]:
+            return 'republican'
+        if prob[0]>prob[1]:
+            return 'democrat';
+        else:
+            return 'republican'
--- project4/question3_solver.py
+++ project4/question3_solver.py
@@ -1,3 +1,4 @@
+import math
 class Question3_Solver:
     def __init__(self):
         return;
@@ -6,6 +7,39 @@ class Question3_Solver:
     # Return the centroids of clusters.
     # You must use [(30, 30), (150, 30), (90, 130)] as initial centroids
     def solve(self, points):
-        centroids = [(30, 60), (150, 60), (90, 130)];
-        return centroids;
-
+        centroids = [[30, 60], [150, 60], [90, 130]]
+        newcentroid = self.update_centroid(centroids,points)
+        while(self.are_unequal(centroids,newcentroid)):
+            centroids = newcentroid
+            newcentroid = self.update_centroid(centroids,points)
+        return newcentroid
+        
+    def dist(self, p1, p2):
+        return math.sqrt(math.pow(p1[0]-p2[0],2) + math.pow(p1[1]-p2[1],2))
+    def update_centroid(self,centroids,points):
+        print "updating centroid"
+        distances = [0.0,0.0,0.0]
+        assignments = []
+        pointsineachcentroid = [0,0,0]
+        for data in points:
+            for i in range(0,3):
+                distances[i] = self.dist(data,centroids[i])
+            assignments.append(distances.index(min(distances)))
+        
+        newcentroids = [[0, 0], [0, 0], [0, 0]]
+        
+        for i in range(0,len(points)):
+            pointsineachcentroid[assignments[i]] += 1
+            newcentroids[assignments[i]][0] += points[i][0]
+            newcentroids[assignments[i]][1] += points[i][1]
+        for i in range(0,3):
+            newcentroids[i][0] /= pointsineachcentroid[i]
+            newcentroids[i][1] /= pointsineachcentroid[i]
+        return newcentroids;
+        
+    def are_unequal(self, p1,p2):
+        booleanarray = []
+        for i in range(0,3):
+            for j in range(0,2):
+                booleanarray.append(p1[i][j] == p2[i][j])
+        return not all(booleanarray)
